9919dd5c5956f34a34cdba768bba3f6c7fec8fc9
cyc <2938287734@qq.com>
2024-01-13 23:03:31 +0800

    feat: 首页

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,63 @@
 <template>
   <div>
-    <h1>首页</h1>
+    <el-card>
+      <div class="card-container">
+        <el-image
+          class="avatar"
+          :src="userStore.userInfo.avatar"
+          :preview-src-list="[userStore.userInfo.avatar]"
+          fit="cover"
+        />
+        <div>
+          <h3 class="time-username">
+            {{ `${getTime()}好${userStore.userInfo.name}` }}
+          </h3>
+          <p class="title">{{ setting.title }}</p>
+        </div>
+      </div>
+    </el-card>
+    <svg-icon name="welcome" width="100%" height="100%" class="svg"></svg-icon>
   </div>
 </template>
 
 <script setup lang="ts">
+import useUserStore from '@/store/modules/user'
+import { getTime } from '@/utils/time'
+import setting from '@/setting'
+
 defineOptions({
   name: 'Home',
 })
+
+const userStore = useUserStore()
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.card-container {
+  display: flex;
+  align-items: center;
+
+  .avatar {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    margin-right: 20px;
+  }
+
+  .time-username {
+    font-size: 20px;
+    font-weight: bold;
+  }
+
+  .title {
+    font-size: 14px;
+    font-style: italic;
+    margin-top: 15px;
+    color: #b5b5b5;
+  }
+}
+
+.svg {
+  margin-top: 50px;
+}
+</style>
